8b644af5830f22e21204f9925317c9a9a443baa6
Colin Sindle <csindle@gmail.com>
2021-09-30 17:31:49 +0200

    Pylint cleanup.

--- script/sanitise.py
+++ script/sanitise.py
@@ -1,46 +1,47 @@
 #!/bin/env python3
 
+"""
+Verify the integrity of CUP files.
+"""
+
 import sys
-import pprint
+# import pprint
 
-from aerofiles.seeyou import Reader, Writer, Converter
+from aerofiles.seeyou import Reader, Writer
 
 
-def convert(wp: dict) -> dict:
+def convert(point: dict) -> dict:
     """
     Hack the aerofiles waypoint input format to its output format.
     :return: a writeable waypoint.
     """
-    wp['shortname'] = wp['code']
-    del wp['code']
+    point['shortname'] = point['code']
+    del point['code']
 
     # Convert dict to tuple:
-    wp['elevation'] = (wp['elevation']['value'], wp['elevation']['unit'])
+    point['elevation'] = (point['elevation']['value'], point['elevation']['unit'])
 
-    if wp['runway_direction'] is not None:
+    if point['runway_direction'] is not None:
         # Convert dict to tuple:
-        wp['runway_length'] = (wp['runway_length']['value'], wp['runway_length']['unit'])
+        point['runway_length'] = (point['runway_length']['value'], point['runway_length']['unit'])
     else:
-        del wp['runway_direction']
-        del wp['runway_length']
-    return wp
+        del point['runway_direction']
+        del point['runway_length']
+    return point
 
 
 def main(in_filename: str, out_filename: str) -> None:
     """
     Sanitise the input XCSoar/SeeYou CUP file, to the output CUP file.
-
-    Notes:
-
     """
-    c = Reader().read(open(in_filename))
-    waypoints = c["waypoints"]
+    cup_file = Reader().read(open(in_filename))
+    waypoints = cup_file["waypoints"]
     print(f'Imported {len(waypoints)} waypoints.')
-    with open(out_filename, 'wb') as fp:
-        writer = Writer(fp)
-        for i, wp in enumerate(waypoints):
-            writeable_wp = convert(wp)
-            #pprint.pprint(writeable_wp)
+    with open(out_filename, 'wb') as out_file:
+        writer = Writer(out_file)
+        for point in waypoints:
+            writeable_wp = convert(point)
+            # pprint.pprint(writeable_wp)
             writer.write_waypoint(**writeable_wp)
         print(f'Written {len(waypoints)} waypoints.')
 
