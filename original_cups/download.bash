532bd44948f2c314e1f24230280576f2c670a277
Colin Sindle <csindle@gmail.com>
2021-10-02 19:17:26 +0200

    Document some CUP file sources.

---
+++ original_cups/download.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+## Some reference CUP files.
+
+
+# IN PROGRESS: https://github.com/XCSoar/xcsoar-data-content/blob/master/waypoints/South_Africa.cup
+curl -OJ http://download.xcsoar.org/waypoints/South_Africa.cup
+
+# IN PROGRESS: https://www.soaringspot.com/en_gb/2021-sa-nats/downloads
+curl -OJ https://www.soaringspot.com/en_gb/download-contest-file/3624-21767
+
+
+
+# https://www.soaringspot.com/en_gb/gauteng-regionals-2021-orient-2021/downloads
+curl -OJ https://archive.soaringspot.com/contest/034/3446/waypoint/18852.cup
+
+## https://www.soaringspot.com/en_gb/wcr2021/downloads
+curl -OJ https://archive.soaringspot.com/contest/033/3315/waypoint/18553.cup
+
+## https://www.soaringspot.com/en_gb/2020-south-african-nationals-welkom-2020/downloads
+curl -OJ https://archive.soaringspot.com/contest/033/3321/waypoint/18347.cup
+
+#################################
+
+## ZA_Cape to be used here:
+#https://www.soaringspot.com/en_gb/western-cape-regionals-cape-gauntlet-jan-2022-worcester-2022/
