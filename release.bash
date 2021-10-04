7693506337c82426b9dbbdf5cbfe9a2d7c1ac0de
Colin Sindle <csindle@gmail.com>
2021-10-04 17:24:34 +0200

    Automate one more release step.

--- release.bash
+++ release.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# Release process documentation (rather that a script).
 
-# TODO: Keep only the first 11 CSV columns.
+mv ~/Downloads/'WaypointsZA - ZA_Cape.cup.csv' ./
+
+# Keep only the first 11 CSV columns (TODO: will fail with quoted and escaped commas! Then use LibreOffice).
+cut -d, -f1-11 'WaypointsZA - ZA_Cape.cup.csv' ZA_Cape.cup.csv
+
 ./script/sanitise.py ZA_Cape.cup.csv  ZA_Cape.cup
 cp ZA_Cape.cup ZA_Cape_$(date +\%Y\%m\%d).cup
 # TODO: git diff, git commit, git push
 # TODO: Release to GitHub
-# TODO: Release to Google Maps
 # TODO: Release to SoaringSpot
 gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup   -o kml -F ZA_Cape_$(date +\%Y\%m\%d).cup.kml
+# TODO: Release to Google Maps
