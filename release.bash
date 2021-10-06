e45f287479c981b7f9fe8da13202ded81f098031
Colin Sindle <csindle@gmail.com>
2021-10-06 13:17:30 +0200

    fix: not responsible for dir maintenance.

--- release.bash
+++ release.bash
@@ -7,20 +7,18 @@ set -e
 ## After running:
 #./script/sanitise.py 'WaypointsZA - ZA_Cape.cup.csv' ZA_Cape.cup
 
+# Output directory
+OD=$1
+
+mkdir -p ${OD}
 
 # Version / timestamp:
 DATE=$(date +\%Y\%m\%d)
-OD="out"
-
-rm -r ${OD}
-mkdir -p ${OD}
 
 cp -f ZA_Cape.cup ${OD}/ZA_Cape_${DATE}.cup    # For SoaringSpot
 cp -f ZA_Cape.cup ${OD}/ZA_Cape_${DATE}.cup.csv   # For GitHub Gist
 gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o geojson -F ${OD}/ZA_Cape_${DATE}.geojson   # For GitHub Gist
 gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o kml     -F ${OD}/ZA_Cape_${DATE}.cup.kml   # For Google Maps
-
-ls -larth ${OD}
 
 # TODO: git diff, git commit, git push
 # TODO: Release to GitHub
