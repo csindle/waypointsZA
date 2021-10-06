8f87248e8d874ed36673117778c8762fafbd5940
Colin Sindle <csindle@gmail.com>
2021-10-06 13:12:35 +0200

    feat: move tasks out of actions to script.

--- release.bash
+++ release.bash
@@ -4,18 +4,23 @@
 
 set -e
 
-./script/sanitise.py 'WaypointsZA - ZA_Cape.cup.csv' ZA_Cape.cup
+## After running:
+#./script/sanitise.py 'WaypointsZA - ZA_Cape.cup.csv' ZA_Cape.cup
 
-### Outputs: ###
 
 # Version / timestamp:
 DATE=$(date +\%Y\%m\%d)
+OD="out"
 
-cp -f ZA_Cape.cup ZA_Cape_${DATE}.cup    # For SoaringSpot
-cp -f ZA_Cape.cup ZA_Cape_${DATE}.cup.csv   # For GitHub Gist
-gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o geojson -F ZA_Cape_${DATE}.geojson   # For GitHub Gist
-gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o kml     -F ZA_Cape_${DATE}.cup.kml   # For Google Maps
+rm -r ${OD}
+mkdir -p ${OD}
 
+cp -f ZA_Cape.cup ${OD}/ZA_Cape_${DATE}.cup    # For SoaringSpot
+cp -f ZA_Cape.cup ${OD}/ZA_Cape_${DATE}.cup.csv   # For GitHub Gist
+gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o geojson -F ${OD}/ZA_Cape_${DATE}.geojson   # For GitHub Gist
+gpsbabel -i xcsv,style=script/name_cup.style -f ZA_Cape.cup -o kml     -F ${OD}/ZA_Cape_${DATE}.cup.kml   # For Google Maps
+
+ls -larth ${OD}
 
 # TODO: git diff, git commit, git push
 # TODO: Release to GitHub
